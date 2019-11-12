0050304dacea81901f038b4172edd7648a2387a7
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-11-12 16:56:47 +0800

    Update Weaponize-Me.psm1

--- Weaponize-Me.psm1
+++ Weaponize-Me.psm1
@@ -1,1 +1,31 @@
 
+#requires -version 3
+<#
+.SYNOPSIS
+  Weaponize-Me 
+.DESCRIPTION
+  Weaponize-Me
+.PARAMETER <Parameter_Name>
+    none
+.INPUTS
+  none
+.OUTPUTS
+  none
+.NOTES
+  Version:        0.7
+  Author:         WillChaos
+  Creation Date:  12-11-19
+  Purpose/Change: Fuck shit up, my way, on my time, with my preference of shell.
+  
+.EXAMPLE
+  none
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
