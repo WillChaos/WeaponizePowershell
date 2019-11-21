731a7385c0f09047ee730f3d4354ccf38f3d5d6c
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-11-21 15:28:28 +0800

    Update Weaponize-Me.psm1

--- Weaponize-Me.psm1
+++ Weaponize-Me.psm1
@@ -1,16 +1,20 @@
-
 #requires -version 3
 <#
 .SYNOPSIS
   Weaponize-Me 
+
 .DESCRIPTION
   Weaponize-Me
-.PARAMETER <Parameter_Name>
-    none
+
+.PARAMETER 
+  None
+
 .INPUTS
-  none
+  None
+
 .OUTPUTS
-  none
+  None
+
 .NOTES
   Version:        0.7
   Author:         WillChaos
@@ -22,10 +26,38 @@
 #>
 
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
+# Configure TLS support for github
+    try
+    {
+        ([Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12) | Out-Null
+        Write-Host "[+] Configured TLS 1.2 Channels for Git communication" -ForegroundColor DarkMagenta
+
+    }
+    catch
+    {
+        Write-Host "[!] Failed to build TLS 1.2 Channel Support" -ForegroundColor DarkRed
+    }
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
+# Location for WPS ZIP Master in gitgub
+    $WPS_MasterLocation = "https://codeload.github.com/WillChaos/WeaponizePowershell/zip/master"
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
+Function WPSInvoke-WPSBanner
+{
+    # Simple banner
+    Write-Host " 
+    
+              _      __                        _         ___                     ______       ____  
+             | | /| / /__ ___ ____  ___  ___  (_)__ ___ / _ \___ _    _____ ____/ __/ /  ___ / / /  
+             | |/ |/ / -_) _ `/ _ \/ _ \/ _ \/ /_ // -_) ___/ _ \ |/|/ / -_) __/\ \/ _ \/ -_) / /   
+             |__/|__/\__/\_,_/ .__/\___/_//_/_//__/\__/_/   \___/__,__/\__/_/ /___/_//_/\__/_/_/    
+                            /_/                                                                     
+
+    " -ForegroundColor Green
+
+}
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
+WPSInvoke-WPSBanner
