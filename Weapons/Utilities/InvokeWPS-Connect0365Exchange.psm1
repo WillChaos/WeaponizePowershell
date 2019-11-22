aa6094908f3b5686749951987c6de80ca730368d
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-11-22 16:37:18 +0800

    Create InvokeWPS-Connect0365Exchange.psm1

---
+++ Weapons/Utilities/InvokeWPS-Connect0365Exchange.psm1
@@ -0,0 +1,24 @@
+Function InvokeWPS-Connect0365Exchange()
+{
+    $Credential = Get-Credential
+
+        try
+        {
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+                                     -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+                                     -Credential $Credential `
+                                     -Authentication Basic `
+                                     -AllowRedirection `
+                                     -ErrorAction Stop `
+                                     -WarningAction SilentlyContinue `
+                                     -InformationAction SilentlyContinue
+
+            Write-Host "[WPS]> Connected to 0365 Exchange!" -ForegroundColor DarkGreen                          
+                                     
+        }
+        catch
+        {
+            Write-Host "[WPS]> Failed to connect to 0365 Exchange - Auth failure?" -ForegroundColor DarkRed
+        }
+ 
+}
