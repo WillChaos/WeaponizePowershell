25ed261ed042f812f7cdb5fb27bfbcefede019a9
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-11-25 14:58:09 +0800

    Update InvokeWPS-Connect0365Exchange.psm1

--- Weapons/Utilities/InvokeWPS-Connect0365Exchange.psm1
+++ Weapons/Utilities/InvokeWPS-Connect0365Exchange.psm1
@@ -1,3 +1,25 @@
+<#
+.SYNOPSIS
+  InvokeWPS-Connect0365Exchange.psm1
+.DESCRIPTION
+  Simply built to connect to office365 exchange admin - nothing fancy here.
+.PARAMETER 
+  None / Script is interactive
+.INPUTS
+  None / Script is interactive
+.OUTPUTS
+  None
+.NOTES
+  Version:        0.7
+  Author:         WillChaos
+  Creation Date:  25/11/19
+  Purpose/Change: Simple Automation.
+  
+.EXAMPLE
+  PS:/> Import-Module InvokeWPS-Connect0365Exchange.psm1
+  PS:/> InvokeWPS-Connect0365Exchange
+#>
+
 Function Global:InvokeWPS-Connect0365Exchange()
 {
     $Credential = Get-Credential
