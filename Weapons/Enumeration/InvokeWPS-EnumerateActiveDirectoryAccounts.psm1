f9d0fb6e29e48c5a11b6fe4f8287d88eefe7a789
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-12-11 11:33:25 +0800

    Create InvokeWPS-EnumerateActiveDirectoryAccounts.psm1

---
+++ Weapons/Enumeration/InvokeWPS-EnumerateActiveDirectoryAccounts.psm1
@@ -0,0 +1,70 @@
+Function Global:InvokeWPS-EnumerateActiveDirectoryAccounts()
+{
+    param
+    (
+
+    [Switch] $PrivUsersOnly,
+    
+    [Switch] $ActivityReport
+    
+    
+    )
+
+    # pre req check
+    if(Get-Module ActiveDirectory)
+    {
+        Import-Module ActiveDirectory -Force -ErrorAction Stop | Out-Null
+        Write-Host "Imported AD module" -ForegroundColor DarkMagenta
+    }
+    else
+    {
+        Write-Host "Error - Unable to import AD module." -ForegroundColor DarkRed
+    }
+
+    # handle enum
+    if($PrivUsersOnly)
+    {
+        Write-Host "[+] Begun enumerating privlaged users in AD" -ForegroundColor DarkGreen
+
+        # get every AD User account object
+        $PrivGroups = @(
+                        "Domain Admins",
+                        "Schema Admins",
+                        "Administrators",
+                        "Hyper-V Administrators",
+                        "Account Operators",
+                        "Backup Operators",
+                        "Server Operators",
+                        "Enterprise Admins",
+                        "Group Policy Creator Owners"
+                        )
+
+        foreach($Group in $PrivGroups)
+        {
+            $GroupMembers = Get-ADGroupMember -Identity $Group
+            if($GroupMembers)
+            {
+                Write-Host " -- " $Group " -- "
+                foreach($ThisUser in $GroupMembers)
+                {
+                    Write-Host "-> " $ThisUser.Name
+                }
+                Write-Host " "
+            }
+        }
+    }
+    elseif($ActivityReport)
+    {
+        Write-Host "[+] Begun building Activity report for all AD users" -ForegroundColor DarkGreen
+
+        Get-ADUser -Filter 'enabled -eq $true' -Properties * | Select-Object -Property Name, 
+            @{Name="Account Created";Expression="WhenCreated"}, 
+            @{Name="Password last reset";Expression="PasswordLastSet"}, 
+            @{Name="LogonCount";Expression="LogonCount"}, 
+            @{Name="Total failed logons";Expression="msDS-FailedInteractiveLogonCount"},
+            @{Name="Recent failed logons";Expression="msDS-FailedInteractiveLogonCountAtLastSuccessfulLogon"}, 
+            @{Name="Last failed logon";Expression={[datetime]::FromFileTime($_.'msDS-LastFailedInteractiveLogonTime')}}, 
+            @{Name="Last successful logon";Expression={[datetime]::FromFileTime($_.'msDS-LastSuccessfulInteractiveLogonTime')}} | 
+        Sort-Object Name | Format-Table
+    }
+}
