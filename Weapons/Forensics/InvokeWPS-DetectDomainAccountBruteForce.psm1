5617774d829083fde2649ea7cf4798e6a6dc01a1
WillChaos <46702070+WillChaos@users.noreply.github.com>
2020-01-24 14:04:32 +0800

    Update InvokeWPS-DetectDomainAccountBruteForce.psm1

--- Weapons/Forensics/InvokeWPS-DetectDomainAccountBruteForce.psm1
+++ Weapons/Forensics/InvokeWPS-DetectDomainAccountBruteForce.psm1
@@ -15,14 +15,17 @@ Function Global:InvokeWPS-DetectDomainAccountBruteForce()
         [String] $Hours
     )
     
-    $ID_LO = @(4740,644,6279)     # Account locked out.
-    $ID_AU = @(4767)              # Account unlocked.
-    $ID_FL = @(4625,529)          # Failed Logon because of bad password	
+    # Event IDS
+    $ID_LO = @(4740,644,6279)                  # Account locked out.
+    $ID_AU = @(4767)                           # Account unlocked.
+    $ID_FL = @(529,4776,4769,4625,4768,4771)  # Failed Logon because of bad password	
    
-    # build custom Audit object
+    # Error codes (WIP)
+    $0xc000006a = ("0xc000006a: username is correct, but the password is wrong")
+
+    # Build custom Audit object
     $OBJ_FL = New-Object -TypeName psobject
    
-
     Write-Host "[WPS]> Checking for potential brute forceing..." -ForegroundColor Green
 
     try
@@ -32,20 +35,106 @@ Function Global:InvokeWPS-DetectDomainAccountBruteForce()
             Write-Host "------------------------ Failed Logons in the past $Hours hours ------------------------------" -ForegroundColor DarkGray
             foreach($log in $result)
             {
-                # get info
-                $name     = (($log).ReplacementStrings)[0]
-                $target   = (($log).ReplacementStrings)[1]
-                $timegen  =   $log.TimeGenerated
-                $timewrit =   $log.TimeWritten
-                $eventid  =   $log.InstanceId
+               #handling formating of different logs
+               if($log.InstanceId -like 4776)
+               {
+                    # get info
+                    $name     = (($log).ReplacementStrings)[1]
+                    $target   = (($log).ReplacementStrings)[2]
+                    $errcode  = (($log).ReplacementStrings)[3]
+                    $timegen  =   $log.TimeGenerated
+                    $timewrit =   $log.TimeWritten
+                    $eventid  =   $log.InstanceId
 
-                # present info
-                Write-Host "[+] $name failed logon"                          -ForegroundColor Green
-                Write-Host "-[*] Time Generated: $timegen"                   -ForegroundColor Gray
-                Write-Host "-[*] Time Written: $timewrit"                    -ForegroundColor Gray
-                Write-Host "-[*] target devcie locking out against: $target" -ForegroundColor Gray
-                Write-Host "-[*] Log Event ID : $eventid"                    -ForegroundColor Gray
-                Write-Host "-"
+                    # present info
+                    Write-Host "[+] $name failed logon"                          -ForegroundColor Green
+                    Write-Host "-[*] Time Generated: $timegen"                   -ForegroundColor Gray
+                    Write-Host "-[*] Time Written: $timewrit"                    -ForegroundColor Gray
+                    Write-Host "-[*] target devcie locking out against: $target" -ForegroundColor Gray
+                    Write-Host "-[*] Log Event ID : $eventid"                    -ForegroundColor Gray
+                    Write-Host "-[*] Logon Error code: : $errcode"               -ForegroundColor Gray
+                    Write-Host "-"
+               }
+               if($log.InstanceId -like 4769)
+               {
+                    # get info
+                    $name     = (($log).ReplacementStrings)[1]
+                    $target   = (($log).ReplacementStrings)[6] + ":" + (($log).ReplacementStrings)[7]
+                    $errcode  = (($log).ReplacementStrings)[8]
+                    $timegen  =   $log.TimeGenerated
+                    $timewrit =   $log.TimeWritten
+                    $eventid  =   $log.InstanceId
+
+                    # present info
+                    Write-Host "[+] $name failed logon"                          -ForegroundColor Green
+                    Write-Host "-[*] Time Generated: $timegen"                   -ForegroundColor Gray
+                    Write-Host "-[*] Time Written: $timewrit"                    -ForegroundColor Gray
+                    Write-Host "-[*] lockout destination: $target"               -ForegroundColor Gray
+                    Write-Host "-[*] Log Event ID : $eventid"                    -ForegroundColor Gray
+                    Write-Host "-[*] Logon Error code: : $errcode"               -ForegroundColor Gray
+                    Write-Host "-"
+               }
+               if($log.InstanceId -like 4625)
+               {
+                    # get info
+                    $name       = (($log).ReplacementStrings)[5]
+                    $target     = (($log).ReplacementStrings)[13] 
+                    $locproc    = (($log).ReplacementStrings)[11]
+                    $errreason  = (($log).ReplacementStrings)[7]
+                    $timegen    =   $log.TimeGenerated
+                    $timewrit   =   $log.TimeWritten
+                    $eventid    =   $log.InstanceId
+                    
+
+                    # present info
+                    Write-Host "[+] $name failed logon"                          -ForegroundColor Green
+                    Write-Host "-[*] Time Generated: $timegen"                   -ForegroundColor Gray
+                    Write-Host "-[*] Time Written: $timewrit"                    -ForegroundColor Gray
+                    Write-Host "-[*] lockout destination: $target"               -ForegroundColor Gray
+                    Write-Host "-[*] Log Event ID : $eventid"                    -ForegroundColor Gray
+                    Write-Host "-[*] Failure Reason: : $errreason"               -ForegroundColor Gray
+                    Write-Host "-[*] Process assosiated with lockout: $locproc"  -ForegroundColor Gray
+                    Write-Host "-"
+               }
+               if($log.InstanceId -like 4771)
+               {
+                    # get info
+                    $name       = (($log).ReplacementStrings)[0]
+                    $target     = (($log).ReplacementStrings)[6] 
+                    $failcode   = (($log).ReplacementStrings)[4]
+                    $timegen    =   $log.TimeGenerated
+                    $timewrit   =   $log.TimeWritten
+                    $eventid    =   $log.InstanceId
+                    
+
+                    # present info
+                    Write-Host "[+] $name failed logon"                          -ForegroundColor Green
+                    Write-Host "-[*] Time Generated: $timegen"                   -ForegroundColor Gray
+                    Write-Host "-[*] Time Written: $timewrit"                    -ForegroundColor Gray
+                    Write-Host "-[*] lockout destination: $target"               -ForegroundColor Gray
+                    Write-Host "-[*] Log Event ID : $eventid"                    -ForegroundColor Gray
+                    Write-Host "-[*] Failure Code: : $failcode"                  -ForegroundColor Gray
+                    Write-Host "-"
+               }
+               else
+               {
+                    # catches some geenric event logs
+                    # get info
+                    $name     = (($log).ReplacementStrings)[0]
+                    $target   = (($log).ReplacementStrings)[1]
+                    $timegen  =   $log.TimeGenerated
+                    $timewrit =   $log.TimeWritten
+                    $eventid  =   $log.InstanceId
+
+                    # present info
+                    Write-Host "[+] $name failed logon"                                         -ForegroundColor Green
+                    Write-Host "-[*] Time Generated: $timegen"                                  -ForegroundColor Gray
+                    Write-Host "-[*] Time Written: $timewrit"                                   -ForegroundColor Gray
+                    Write-Host "-[*] target devcie locking out against: $target"                -ForegroundColor Gray
+                    Write-Host "-[*] Log Event ID : $eventid"                                   -ForegroundColor Gray
+                    Write-Host "NOTE: This event hasnt been coded into the script properly yet" -ForegroundColor Gray
+                    Write-Host "-"
+               }
             }
             
         }
