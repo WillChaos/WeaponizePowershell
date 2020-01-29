073e0f0cf25bbc1fb7d3d904b8e04be7f0951d7e
WillChaos <46702070+WillChaos@users.noreply.github.com>
2020-01-29 17:03:02 +0800

    Create InvokeWPS-EnumerateDomain.psm1
    not even close to being finished.

---
+++ Weapons/Enumeration/InvokeWPS-EnumerateDomain.psm1
@@ -0,0 +1,56 @@
+Function Global:InvokeWPS-EnumerateDomain()
+{
+    param
+    (
+        [Parameter(Position=0,mandatory=$true)]
+        [string] $DomainName
+    )
+
+    # MX 
+    Write-Host "[+] Resolving Mail Exchange for target: $DomainName " -ForegroundColor Magenta
+    foreach($MXRecord in (Resolve-DnsName -Name $DomainName -Type MX -ErrorAction SilentlyContinue))
+    {
+        if($MXRecord.section -like "Answer")
+        {
+            $MXPriority = $MXRecord.Preference
+            $MXName     = $MXRecord.Name
+            $MXNE       = $MXRecord.NameExchange
+            $MXIP       = (Resolve-DnsName -Name $MXNE -Type A -ErrorAction SilentlyContinue).IP4Address
+
+            Write-Host "-[$MXPriority] $MXName | $MXNE > $MXIP" -ForegroundColor DarkGray
+        }
+    }
+
+
+    # WWW / @ 
+    Write-Host "[+] Resolving webhost information: $DomainName " -ForegroundColor Magenta
+    foreach($ARecord in (Resolve-DnsName -Name $DomainName -Type A -ErrorAction SilentlyContinue))
+    {
+        if($ARecord.section -like "Answer")
+        {
+            $AName     = $ARecord.Name
+            $AIP       = $ARecord.IP4Address
+            $ASN       = (wget https://api.hackertarget.com/aslookup/?q=$AIP -ErrorAction SilentlyContinue)
+
+            Write-Host "-[A] $AName > $AIP | ($ASN)" -ForegroundColor DarkGray
+        }
+
+    }
+    foreach($ARecord in (Resolve-DnsName -Name "www.$DomainName" -Type A -ErrorAction SilentlyContinue))
+    {
+        if($ARecord.section -like "Answer")
+        {
+            $AName     = $ARecord.Name
+            $AIP       = $ARecord.IP4Address
+            $ASN       = (wget https://api.hackertarget.com/aslookup/?q=$AIP -ErrorAction SilentlyContinue)
+            Write-Host "-[A] $AName > $AIP | ($ASN)" -ForegroundColor DarkGray
+        }
+
+    }
+
+    # .TXT / _DMARC / _DKIM
+
+    # Whois
+
+    return $EnumOBJ
+}
