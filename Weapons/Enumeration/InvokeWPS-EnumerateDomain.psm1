1f2c59d03e1a303e2aa38aa24142bd9474b82657
WillChaos <46702070+WillChaos@users.noreply.github.com>
2020-02-05 17:41:10 +0800

    Update InvokeWPS-EnumerateDomain.psm1

--- Weapons/Enumeration/InvokeWPS-EnumerateDomain.psm1
+++ Weapons/Enumeration/InvokeWPS-EnumerateDomain.psm1
@@ -17,7 +17,7 @@ Function Global:InvokeWPS-EnumerateDomain()
             $MXNE       = $MXRecord.NameExchange
             $MXIP       = (Resolve-DnsName -Name $MXNE -Type A -ErrorAction SilentlyContinue).IP4Address
             $ASN        = (wget https://api.hackertarget.com/aslookup/?q=$MXIP -ErrorAction SilentlyContinue)
-            Write-Host "-[$MXPriority] $MXName | $MXNE > $MXIP ($ASN)" -ForegroundColor DarkGray
+            Write-Host "-[MX: $MXPriority] $MXName | $MXNE > $MXIP ($ASN)" -ForegroundColor DarkGray
         }
     }
 
@@ -48,7 +48,82 @@ Function Global:InvokeWPS-EnumerateDomain()
 
     }
 
-    # .TXT / _DMARC / _DKIM
+    # NS
+    Write-Host "[+] Resolving nameserver information: $DomainName " -ForegroundColor Magenta
+    foreach($NSRecord in (Resolve-DnsName -Name $DomainName -Type NS -ErrorAction SilentlyContinue))
+    {
+        if($NSRecord.section -like "Answer")
+        {
+            $NSName     = $NSRecord.Name
+            $NSIP       = $NSRecord.NameHost 
+
+            Write-Host "-[NS] $NSName > $NSIP" -ForegroundColor DarkGray
+        }
+
+    }
+
+    # .TXT 
+    Write-Host "[+] Resolving TXT information: $DomainName " -ForegroundColor Magenta
+    foreach($TXTRecord in (Resolve-DnsName -Name $DomainName -Type txt -ErrorAction SilentlyContinue))
+    {
+        if($TXTRecord.section -like "Answer")
+        {
+            $TXTName     = $TXTRecord.Name
+            $TXTEntry    = $TXTRecord.Strings 
+
+            Write-Host "-[TXT] $TXTName > $TXTEntry" -ForegroundColor DarkGray
+        }
+
+    }
+    # ._DMARC 
+    Write-Host "[+] Resolving _DMARC information: $DomainName " -ForegroundColor Magenta
+    foreach($DMARCRecord in (Resolve-DnsName -Name "_Dmarc.$DomainName" -Type txt -ErrorAction SilentlyContinue))
+    {
+        if($DMARCRecord.section -like "Answer")
+        {
+            $DMARCName     = $DMARCRecord.Name
+            $DMARCEntry    = $DMARCRecord.Strings 
+
+            Write-Host "-[TXT] $DMARCName > $DMARCEntry" -ForegroundColor DarkGray
+        }
+
+    }
+    # DKIM (this isnt reliable - brute forcey)
+    Write-Host "[+] Attempting to resolve DKIM information: $DomainName (not 100% reliable" -ForegroundColor Magenta
+    # 365
+    if(Resolve-DnsName -Name selector1._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] Office365 DKIM signing detected: selector1._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    if(Resolve-DnsName -Name selector2._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] Office365 DKIM signing detected: selector2._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    if(Resolve-DnsName -Name k1._domainkey._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] Mailchimp DKIM signing detected: k1._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    if(Resolve-DnsName -Name cm._domainkey._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] e-Campaign DKIM signing detected: cm._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    if(Resolve-DnsName -Name s1._domainkey._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] SendGrid DKIM signing detected: s1._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    if(Resolve-DnsName -Name s2._domainkey._domainkey.$DomainName -ErrorAction SilentlyContinue)
+    {
+        Write-Host "-[DKIM] SendGrid DKIM signing detected: s2._domainkey.$DomainName" -ForegroundColor DarkGray
+
+    }
+    
+    
+
 
     # Whois
 }
