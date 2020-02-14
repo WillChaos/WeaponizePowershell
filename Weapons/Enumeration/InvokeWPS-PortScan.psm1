9b2c1f9fb2f83eda88da974a651890ea01362b42
WillChaos <46702070+WillChaos@users.noreply.github.com>
2020-02-14 21:28:35 +0800

    Update InvokeWPS-PortScan.psm1
    WIP

--- Weapons/Enumeration/InvokeWPS-PortScan.psm1
+++ Weapons/Enumeration/InvokeWPS-PortScan.psm1
@@ -1,20 +1,48 @@
 <#
+.SYNOPSIS
+  Simple and reliable port scanner built with powerrshell at the fingertips, 
+  Built with Masscan as the backbone for speed and NMAP for the reliable inspection.
+
+.DESCRIPTION
+  Port Scanner and service detector. this will also use some of Nmaps NSE engine to find default vulns.
+  This shouldnt be reliaed on for deep port inspection - just as a reliable indicator of weather a port is open or not.
+
+.PARAMETER 
+  $RHost         : Remote Host to target 
+  $SurfaceScan   : Extensive scan. As quick as an in depth initial scan can be. masscan + indepth Nmap     
+  $QuickEnumScan : Scans top known ports using masscan - then enums those ports using nmap
+  $VeryQuickScan : No Enum/discovery scripts - blast with masscan (less reliable)
+  
+.INPUTS
+  N/A - None Required.
+
+.OUTPUTS
+  N/A - Unless piped and specified otherwise. 
+
 .TargetOS
   OS:NIX
+
+.NOTES
+  Version:        0.7
+  Author:         WillChaos
+  Creation Date:  14-2-2020
+  Purpose/Change: Box Port Enumeration. Efficventrly and conistently. 
+  
+.EXAMPLE
+  InvokeWPS-PortScan -RHost 10.10.10.171 -VeryQuickScan
 #>
 
 Function Global:InvokeWPS-PortScan()
 {
 	param
 	(
-		# extensive scan. As quick as an in depth initial scan can be. masscan + indepth Nmap
+		[Parameter(Mandatory=$true)]
+		[String] $RHost,
+
 		[switch] $SurfaceScan,
-		# Scans top known ports using masscan - then enums those ports using nmap
 		[switch] $QuickEnumScan,
-		# No Enum/discovery scripts - blast with masscan (less reliable)
-		[switch] $VeryQuickScan,
-		# Target
-		[String] $RHost
+		[switch] $VeryQuickScan
+        
 	)
 
 	# Functions
@@ -22,8 +50,8 @@ Function Global:InvokeWPS-PortScan()
 	{
 		if( ((Invoke-Expression -Command "masscan -h") -like "*list those options that are compatible with nmap*") )
 		{
-			# nested ifs just to prevent super long if statements
-			if( (Invoke-Expression -Command "nmap -h") -like "*Usage: nmap [Scan Type(s)]*")
+			# nested if's just to prevent super long if statements
+			if( (Invoke-Expression -Command "nmap -h") -like "*https://nmap.org/book/man.html*")
 			{
 				Write-Host "[WPS] PreReqs Met"
 				return $true
@@ -48,12 +76,15 @@ Function Global:InvokeWPS-PortScan()
 		if($VeryQuickScan -and $RHost)
 		{
 			Write-Host "[+]> Performing Very quick scan" -ForeGroundColor DarkGray
+            $ScanArgs = "sudo masscan --ports 0-65535 $RHost --rate 1000"
+
+            Invoke-Expression -Command $ScanArgs
 		}
 		if($QuickEnumScan -and $RHost)
 		{
 			Write-Host "[+]> Perfroming A Quick Scan + Quick Enum" -ForeGroundColor DarkGray
 		}
-		if(SurfaceScan -and $RHost)
+		if($SurfaceScan -and $RHost)
 		{
 			Write-Host "[+]> Perfroming A Surface scan. This may take a bit.. Grab a coffee.." -ForeGroundColor DarkGray
 		}
